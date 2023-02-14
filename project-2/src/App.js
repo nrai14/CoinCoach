89c352ae4705bc7b8655498bb4f90376b8dd8440
nrai14 <nishadrai14@gmail.com>
2023-02-14 15:01:04 +0000

    set up project properly

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState } from 'react';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
