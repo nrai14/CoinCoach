d1a46f927f2653190d9eb56e605118ca26b2562d
SLM <sabrina.martorelli@gmail.com>
2023-02-15 14:59:35 +0000

    Adds page folder, empty pages and code for NotFound page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,10 @@
+function NotFound() {
+    return (
+        <>
+            <p style={styles.alert}>Page not found.</p>
+            <p style={styles.text}>The URL doesn't exist on the site.</p>
+        </>
+    )
+};
+
+export default NotFound;
