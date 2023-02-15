fcd221bb8a599690e851526b1e0207b4e961847a
SLM <sabrina.martorelli@gmail.com>
2023-02-15 15:02:08 +0000

    Amends Not Found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,8 @@
 function NotFound() {
     return (
         <>
-            <p style={styles.alert}>Page not found.</p>
-            <p style={styles.text}>The URL doesn't exist on the site.</p>
+            <p >Page not found.</p>
+            <p >The URL doesn't exist on the site.</p>
         </>
     )
 };
