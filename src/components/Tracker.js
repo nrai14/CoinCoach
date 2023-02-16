891e355e9b6d7ed1cfe1bd8f301088520ce9696f
SLM <sabrina.martorelli@gmail.com>
2023-02-16 19:45:46 +0000

    Add code for date picker and format code

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import {useState} from 'react';
 
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
@@ -9,6 +9,7 @@ import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
+import Typography from '@mui/material/Typography';
 
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
@@ -27,26 +28,24 @@ function Tracker() {
 
   return (
     <>
-
+   <Typography variant='h2' align='center'>Add Transaction</Typography>
       <Card style={{ maxWidth: 500, margin: "0 auto", padding: "20px 5px" }} >
         <CardContent>
           <form>
             <Grid container spacing={4} justifyContent="center" padding={10}>
 
-
-            <Grid item xs={12}>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker
-              label="Date"
-              value={value}
-              onChange={(newValue) => {
-                setValue(newValue);
-              }}
-              renderInput={(params) => <TextField {...params} />}
-             />
-              </LocalizationProvider>
+            <Grid item xs={12} >
+              <LocalizationProvider dateAdapter={AdapterDayjs} >
+              <DatePicker
+                label="Date" 
+                value={value}
+                onChange={(newValue) => {
+                  setValue(newValue);
+                }}
+                renderInput={(params) => <TextField fullWidth {...params} />}
+              />
+             </LocalizationProvider>
              </Grid>
-
 
               <Grid item xs={12}>
                 <FormControl fullWidth>
@@ -57,28 +56,40 @@ function Tracker() {
                     value={category}
                     label="Category"
                     onChange={handleChange}
+                    required
                   >
                     <MenuItem value={1}>Salary</MenuItem>
                     <MenuItem value={2}>Food</MenuItem>
                     <MenuItem value={3}>Petrol</MenuItem>
                   </Select>
                 </FormControl>
-
-              </Grid>
-
-
-
-              <Grid item xs={12}>
-                <TextField id='' label='Description' Placeholder='Enter description' variant='outlined' fullWidth required />
-              </Grid>
-
-
-              <Grid item xs={12}>
-                <TextField type='number' id='' label='Value' Placeholder='Please enter the value' variant='outlined' fullWidth required />
               </Grid>
 
               <Grid item xs={12}>
-                <Button type="submit" variant='contained' fullWidth> Add Transaction </Button>
+                <TextField id='' 
+                label='Description' 
+                Placeholder='Enter description' 
+                variant='outlined' 
+                fullWidth 
+                required />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField 
+                type='number' 
+                id='' 
+                label='Value' 
+                Placeholder='Please enter the value' 
+                variant='outlined' 
+                fullWidth 
+                required />
+              </Grid>
+
+              <Grid item xs={12}>
+                <Button 
+                type="submit" 
+                variant='contained' 
+                fullWidth> Add Transaction </Button>
               </Grid>
 
             </Grid>
