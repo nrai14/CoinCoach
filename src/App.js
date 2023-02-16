44677f54d19c5e07d69a6522ad8d276912fa8a69
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-02-16 16:08:01 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-
 import {Routes, Route} from "react-router-dom";
-
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Form from './components/Form';
-import OpenAi from './components/OpenAi';
-import CryptoInfo from './components/CryptoInfo';
-
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -17,14 +11,14 @@ function App() {
    
    <>
     <Header />
-    <OpenAi />
+    
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
          <Route path="*" element={<NotFound />} />
       </Routes>
-      <CryptoInfo  />
+      
     <Footer/>
    </>
    
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import Tracker from "../components/Tracker";
-
+import OpenAi from '../components/OpenAi';
+import CryptoInfo from '../components/CryptoInfo';
 
 function Home() {
     return (
         <>
             <h1>Home</h1>
-            <Tracker/>
-
+            {/* <Tracker/> */}
+            <OpenAi />
+            <CryptoInfo  />
+            
         </>
     )
 };
