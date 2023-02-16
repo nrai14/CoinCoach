4f9ab7dc3ebd83716e28a463442e940f7d5bbac3
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-02-17 00:16:26 +0100

    api for crypto working

--- src/components/CryptoInfo.js
+++ src/components/CryptoInfo.js
@@ -1,24 +1,39 @@
+import React, {useState, useEffect} from 'react';
 
-
-
-
+import OpenAi from './OpenAi';
 
 function CryptoInfo() {
+  
+    const [cryptoData, setCryptoData] = useState([]);
+    const api_key = 'f237a2061812440599b5684a5ae72bc9';
+    const crypto_name = 'bitcoin';
+    
+    useEffect(() => {
+      
+      fetch(`https://newsapi.org/v2/everything?q=${crypto_name}&apiKey=${api_key}`)
+        .then(res => res.json())
+        .then(data => setCryptoData(data.articles))
+        console.log(cryptoData);
+    
+    }, [])
+    
+    return (
+      <div>
+        <h1>Crypto Info</h1>
+        <p>Here is some info about crypto</p>
+        {cryptoData.slice(0, 5).map((article, index) => (
+            
+          <div key={index}>
 
 
-
-return (
-
-
-
-<div>
-    <h1>Crypto Info</h1>
-    <p>Here is some info about crypto</p>
-</div>
-
-
-
-);
-}
-
+            <h2>{article.title}</h2>
+            <p>{article.description}</p>
+            <p>{article.url}</p>
+          </div>
+          
+        ))}
+      </div>
+    );
+  }
+  
 export default CryptoInfo;
