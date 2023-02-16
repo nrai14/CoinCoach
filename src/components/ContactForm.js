f86dbefd8c7374d916d75d562a7c3b3445359421
SLM <sabrina.martorelli@gmail.com>
2023-02-16 15:24:56 +0000

    Amends code for contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,36 +1,42 @@
 
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 
 
 function ContactForm() {
-    return (
-        <>
-           
+  return (
+    <>
+      <Card style={{ maxWidth: 500, margin: "0 auto", padding: "20px 5px" }} >
+        <CardContent>
+          <form>
+            <Grid container spacing={4} justifyContent="center" padding={10}>
+              <Grid item xs={12}>
+                <TextField id='' label='Full Name' Placeholder='Enter full name' variant='outlined' fullWidth />
+              </Grid>
 
-        <form>
+              <Grid item xs={12}>
+                <TextField type='email' id='' label='Email' Placeholder='Enter email' variant='outlined' fullWidth required />
+              </Grid>
 
-          <Grid container spacing={1} justifyContent="center" padding={10}>
-          <Grid item xs> 
-          <TextField id='' label='Full Name' Placeholder='Enter full name' variant='outlined' fullWidth />
-          </Grid>
+              <Grid item xs={12}>
+                <TextField id='' label='Message' multiline rows={4} Placeholder='Please type your message here' variant='outlined' fullWidth required />
+              </Grid>
 
-          <Grid item xs> 
-          <TextField type='email' id='' label='Email' Placeholder='Enter email' variant='outlined' fullWidth required />
-          </Grid>
+              <Grid item xs={12}>
+                <Button type="submit" variant='contained' fullWidth> Submit </Button>
+              </Grid>
 
-          <Grid item xs> 
-          <TextField id='' label='Message' multiline rows={4} Placeholder='Please type your message here' variant='outlined' fullWidth required />
-          </Grid>
+            </Grid>
+          </form>
+        </CardContent>
+      </Card>
 
-          </Grid>
-
-        </form>
-
-
-        </>
-    )
+    </>
+  )
 };
 
 export default ContactForm;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ import Typography from '@mui/material/Typography';
 function Contact() {
     return (
         <>
-          <Typography variant='h1' align='center'> Contact Us</Typography>
+          <Typography variant='h2' align='center'> Contact Us</Typography>
           <ContactForm/>
 
         </>
