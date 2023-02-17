fcc692c6cca8c617609377360dc29fb1f9991691
SLM <sabrina.martorelli@gmail.com>
2023-02-17 15:38:41 +0000

    Amends InitPots

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
@@ -20,7 +20,7 @@ function AddTransaction() {
 
 
 
-  
+
   const [transactions, setTransactions] = useState([]);
 
   const [date, setDate] = useState(null);
@@ -28,43 +28,39 @@ function AddTransaction() {
     category: '',
     description: '',
     value: ''
-});
+  });
 
 
 
 
   const handleChange = (event) => {
-    
- 
+
+
     setFormData({
       ...formData,
       [event.target.name]: event.target.value
-  });
+    });
 
   };
 
 
   const handleSubmit = (event) => {
-  
-   event.preventDefault(); 
-   const newTransactions =[
-   {
-    date: 'date',
-    category:'Holidays',
-    description: 'Roma ',
-    value: '400'
-   }, 
-   {
-    date: 'date',
-    category:'Holidays',
-    description: 'Paris ',
-    value: '200'
-   }
-   ];
-   console.log(newTransactions);
-   alert('hola');
-   setTransactions(newTransactions);
-   //localStorage.setItem('transactions', JSON.stringify(transactions));
+
+    event.preventDefault();
+    const newTransactions = [
+      {
+        date: 'date',
+        category: 'Holidays',
+        description: 'Roma ',
+        value: '400'
+      }
+    ];
+
+   
+
+
+    setTransactions(newTransactions);
+    localStorage.setItem('transactions', JSON.stringify(transactions));
 
 
   }
@@ -72,26 +68,26 @@ function AddTransaction() {
 
   return (
     <>
-   <Typography variant='h2' align='center'>Add Transaction</Typography>
+      <Typography variant='h2' align='center'>Add Transaction</Typography>
       <Card style={{ maxWidth: 500, margin: "0 auto", padding: "20px 5px" }} >
         <CardContent>
           <form onSubmit={handleSubmit}>
             <Grid container spacing={4} justifyContent="center" padding={10}>
 
-            <Grid item xs={12} >
-              <LocalizationProvider  dateAdapter={AdapterDayjs} >
-              <DatePicker  
-                label="Date" 
-                value={date}
-               
-                 onChange={(newValue) => {
-                   setDate(newValue);
-                 }}
-              
-                renderInput={(params) => <TextField fullWidth required {...params} />}
-              />
-             </LocalizationProvider>
-             </Grid>
+              <Grid item xs={12} >
+                <LocalizationProvider dateAdapter={AdapterDayjs} >
+                  <DatePicker
+                    label="Date"
+                    value={date}
+
+                    onChange={(newValue) => {
+                      setDate(newValue);
+                    }}
+
+                    renderInput={(params) => <TextField fullWidth required {...params} />}
+                  />
+                </LocalizationProvider>
+              </Grid>
 
               <Grid item xs={12}>
                 <FormControl fullWidth required>
@@ -101,7 +97,7 @@ function AddTransaction() {
                     label="Category"
                     name="category"
                     value={formData.category}
-                    
+
                     onChange={handleChange}>
                     <MenuItem value={0}>Bills</MenuItem>
                     <MenuItem value={1}>Charity</MenuItem>
@@ -119,35 +115,35 @@ function AddTransaction() {
               </Grid>
 
               <Grid item xs={12}>
-                <TextField 
-                name='description' 
-                value={formData.description}
-                label='Description' 
-                placeholder='Enter description' 
-                variant='outlined' 
-                onChange={handleChange}
-                fullWidth 
-                required />
+                <TextField
+                  name='description'
+                  value={formData.description}
+                  label='Description'
+                  placeholder='Enter description'
+                  variant='outlined'
+                  onChange={handleChange}
+                  fullWidth
+                  required />
               </Grid>
 
               <Grid item xs={12}>
-                <TextField 
-                type='number' 
-                name='value' 
-                value= {formData.value}
-                label='Value' 
-                placeholder='Please enter the value' 
-                variant='outlined' 
-                onChange={handleChange}
-                fullWidth 
-                required />
+                <TextField
+                  type='number'
+                  name='value'
+                  value={formData.value}
+                  label='Value'
+                  placeholder='Please enter the value'
+                  variant='outlined'
+                  onChange={handleChange}
+                  fullWidth
+                  required />
               </Grid>
 
               <Grid item xs={12}>
-                <Button 
-                type="submit" 
-                variant='contained' 
-                fullWidth> Add Transaction </Button>
+                <Button
+                  type="submit"
+                  variant='contained'
+                  fullWidth> Add Transaction </Button>
               </Grid>
 
             </Grid>
--- src/components/InitPots.js
+++ src/components/InitPots.js
@@ -6,79 +6,77 @@ function InitPots() {
     const [pots, setPots] = useState([]);
 
     useEffect(() => {
-  
-    const newPots = [
-        {
-            id: 0,
-            category: 'Bills',
-            value: 0
-        },
-        {
-            id: 1,
-            category: 'Charity',
-            value: 0
-        },
-        {
-            id: 2,
-            category: 'Eating Out',
-            value: 0
-        },
-        {
-            id: 3,
-            category: 'Entertainment',
-            value: 0
-        },
-        {
-            id: 4,
-            category: 'Expenses',
-            value: 0
-        },
-        {
-            id: 5,
-            category: 'Groceries',
-            value: 0
-        },
-        {
-            id: 6,
-            category: 'Holidays',
-            value: 0
-        },
-        {
-            id: 7,
-            category: 'Income',
-            value: 0
-        },
-        {
-            id: 8,
-            category: 'Savings',
-            value: 0
-        },
-        {
-            id: 9,
-            category: 'Shopping',
-            value: 0
-        },
-        {
-            id: 10,
-            category: 'Transport',
-            value: 0
-        },
-        {
-            id: 11,
-            category: 'Balance',
-            value: 0
+        const newPots = [
+            {
+                id: 0,
+                category: 'Bills',
+                value: 0
+            },
+            {
+                id: 1,
+                category: 'Charity',
+                value: 0
+            },
+            {
+                id: 2,
+                category: 'Eating Out',
+                value: 0
+            },
+            {
+                id: 3,
+                category: 'Entertainment',
+                value: 0
+            },
+            {
+                id: 4,
+                category: 'Expenses',
+                value: 0
+            },
+            {
+                id: 5,
+                category: 'Groceries',
+                value: 0
+            },
+            {
+                id: 6,
+                category: 'Holidays',
+                value: 0
+            },
+            {
+                id: 7,
+                category: 'Income',
+                value: 0
+            },
+            {
+                id: 8,
+                category: 'Savings',
+                value: 0
+            },
+            {
+                id: 9,
+                category: 'Shopping',
+                value: 0
+            },
+            {
+                id: 10,
+                category: 'Transport',
+                value: 0
+            },
+            {
+                id: 11,
+                category: 'Balance',
+                value: 0
+            }
+
+        ];
+
+        //If there are pots on the Local storage already do not create again
+        const existingPots = JSON.parse(localStorage.getItem('pots'));
+    
+        if (existingPots===null) {     
+            setPots(newPots);
+            localStorage.setItem('pots', JSON.stringify(pots));  
         }
-
-    ];
-
-    //If there are pots on the Local storage already do not create again
-    const existingPots = JSON.parse(localStorage.getItem('pots'));
- 
-    if (!existingPots) {
-        alert('nopots');
-        setPots(newPots);
-        localStorage.setItem('pots', JSON.stringify(pots));  
-    }
     });
 
 
