0328f1e28ae2396ea2b6097967a492f6e91d3cd4
SLM <sabrina.martorelli@gmail.com>
2023-02-17 14:51:21 +0000

    Amends initPots

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {Routes, Route} from "react-router-dom";
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Form from './components/Form';
 import OpenAi from './OpenAi';
 
 import Home from "./pages/Home";
--- src/components/InitPots.js
+++ src/components/InitPots.js
@@ -79,6 +79,7 @@ function InitPots() {
 
 }
 
+export default InitPots;
 
 
 
