d2fe5902fb778d10efd30a246855f96e006be4ab
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-02-18 04:08:01 +0100

    final adjustments to the crypto info component

--- src/components/CryptoInfo.js
+++ src/components/CryptoInfo.js
@@ -6,7 +6,7 @@ function CryptoInfo({cryptoData}) {
     return (
       <div>
         <h3>Short Crypto news</h3>
-        {cryptoData.slice(0, 1).map((article, index) => (
+        {cryptoData.slice(0, 5).map((article, index) => (
           <div key={index}>
             <h4>{article.title}</h4>
             <p>{article.description}</p>
