1d006cf358e41440fd3221b6ed898600694276aa
nrai14 <nishadrai14@gmail.com>
2023-02-19 22:35:24 +0000

    updated about me section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,14 @@
+
+
 function Footer() {
     return (
       <>
+      <div class="footer">
+      <a href="https://github.com/nrai14/CoinCoach"><i class="fa-brands fa-github fa-3x githublogo"></i></a>
+
        <h1> © 2023 CoinCoach</h1>
          
-     
+     </div>
       </>
     );
   }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,63 @@
 function About() {
-    return (
-        <>
-            <h1 >About</h1>
-         
-        </>
-    )
-};
+  return (
+    <>
+      <h1 class="about-title" >About</h1>
+     
+      <div class="container">
+        <p>
+          Welcome to CoinCoach, a React app created by Nish, Sabrina, Michal and
+          Laura as part of their EDX coding bootcamp's final project. CoinCoach
+          is a versatile tool that offers users two crucial functionalities.
+        </p>
+        <div>
+            <img src="Nish.jpeg"></img>
+            <img src="Sabrina.jpeg"></img>
+            <img src="Michal.jpeg"></img>
+            <img src="Laura.jpeg"></img>
+        </div>
+
+        <p>
+          Firstly, CoinCoach serves as a Financial Tracker, making it simple for
+          users to monitor their spending and expenditures against their income.
+          With a user-friendly interface, CoinCoach enables you to track and
+          categorize your expenses, helping you get a better understanding of
+          your financial situation. Our Financial Tracker is designed to be
+          simple, easy to use and gives you a clear picture of your financial
+          health.
+        </p>
+        <p>
+          Secondly, CoinCoach is also a Crypto Currency Tracker, giving you
+          insights on the coins that are worth investing in. Michal worked hard
+          to build our API for this feature, and with his contribution,
+          CoinCoach provides you with real-time information about
+          cryptocurrencies, helping you make informed decisions about your
+          investments.
+        </p>
+        <p>
+          Nish's designed the front page, which sets the tone for the whole user
+          experience. Sabrina worked tirelessly to create the Financial Tracker
+          functionality, making sure it meets user expectations. Laura's visual
+          displays showcase the data from the tracker and make it easy for users
+          to understand and interpret the information. Whilst Michal ensured
+          that the Crypto currency side intertwined well with the Open AI to
+          assist users in understanding the Crypto currency side better.
+        </p>
+        <p>
+          To create this app, we utilized a range of tools such as React, which
+          helped us build an intuitive user interface, and Material UI, which
+          provided us with pre-built components to save time and streamline our
+          design process. We also followed Agile methodology to ensure we could
+          keep track of our progress and respond to feedback quickly.
+        </p>
+        <p>
+          Overall, CoinCoach is a must-have app for anyone who wants to improve
+          their financial literacy and make informed decisions about their
+          investments. We hope you enjoy using it as much as we enjoyed creating
+          it.
+        </p>
+      </div>
+    </>
+  );
+}
 
 export default About;
