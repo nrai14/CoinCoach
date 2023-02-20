f459f22f042676f9f82080dad607b809de9853dc
SLM <sabrina.martorelli@gmail.com>
2023-02-20 01:27:07 +0000

    Amends renderTrans to include banner when is no transaction yet

--- src/components/RenderTransactions.js
+++ src/components/RenderTransactions.js
@@ -4,68 +4,92 @@ import Typography from '@mui/material/Typography';
 import { DataGrid } from '@mui/x-data-grid';
 import Box from '@mui/material/Box';
 
+import { useState, useEffect } from 'react';
 
 
 function RenderTransactions() {
 
-
-  const rows = JSON.parse(localStorage.getItem('transactions'));
+  const [text, setText]= useState('');
+  
 
 
   // const rows = JSON.parse(localStorage.getItem('pots'));
 
 
-  const columns = [
-    {
-      field: 'id',
-      headerName: '#',
-      width: 10
-    },
-    {
-      field: 'date',
-      headerName: 'Date',
-      width: 160,
-      editable: false,
-    },
-    {
-      field: 'category',
-      headerName: 'Category',
-      width: 160,
-      editable: false,
-    },
-    {
-      field: 'description',
-      headerName: 'Description',
-      width: 160,
-      editable: false,
-    },
-    {
-      field: 'value',
-      headerName: 'Value',
-      type: 'number',
-      width: 160,
-      editable: false,
-
-    },
-  ];
 
 
+  useEffect(() => {
+    const rows = JSON.parse(localStorage.getItem('transactions'));
+
+    const columns = [
+      {
+        field: 'id',
+        headerName: '#',
+        width: 10
+      },
+      {
+        field: 'date',
+        headerName: 'Date',
+        width: 160,
+        editable: false,
+      },
+      {
+        field: 'category',
+        headerName: 'Category',
+        width: 160,
+        editable: false,
+      },
+      {
+        field: 'description',
+        headerName: 'Description',
+        width: 160,
+        editable: false,
+      },
+      {
+        field: 'value',
+        headerName: 'Value',
+        type: 'number',
+        width: 160,
+        editable: false,
+  
+      },
+    ];
+  
+    if(rows) {
+
+      setText(<Box sx={{ height: '90%', width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={10}
+        rowsPerPageOptions={[5]}
+        disableSelectionOnClick
+        experimentalFeatures={{ newEditingApi: true }}
+      />
+    </Box> );
+  
+    }
+    else {
+  
+      setText(<Typography variant='h4' align='center'>There is no history yet.</Typography>);
+  
+    }
+  
+  },[]);
+
+
+
+  
 
   return (
     <>
-
       <Typography variant='h6' align='center'>Transactions History</Typography>
+      
+     {text}
+       
+    
 
-      <Box sx={{ height: '90%', width: '100%' }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          pageSize={10}
-          rowsPerPageOptions={[5]}
-          disableSelectionOnClick
-          experimentalFeatures={{ newEditingApi: true }}
-        />
-      </Box>
+    
 
 
     </>
