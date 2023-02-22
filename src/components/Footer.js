c4736745b3e03ab27066f60c48a5ef91dd0691ee
SLM <sabrina.martorelli@gmail.com>
2023-02-22 20:57:23 +0000

    Adds target="_blank" on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <>
       <div className="footer">
-        <a href="https://github.com/nrai14/CoinCoach">
+        <a target="_blank" href="https://github.com/nrai14/CoinCoach">
           <i className="fa-brands fa-github fa-3x githublogo"></i></a>
         <h2 className="Copyright"> © 2023 CoinCoach</h2>
       </div>
