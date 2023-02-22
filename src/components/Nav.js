de7229dc2b73c03447c3e7c8b955466019142125
nrai14 <nishadrai14@gmail.com>
2023-02-22 15:30:01 +0000

    centered the navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ import {Link} from "react-router-dom";
 
 export default function BasicButtons() {
   return (
-    <Stack spacing={2} direction="row" justifyContent="center">
+    <Stack spacing={2} direction="row" justifyContent="center" ml="13px">
       <Button variant="outlined" component={Link} to="/" size="large">Home</Button>
       <Button variant="outlined" component={Link} to="/about" size="large">About</Button>
       <Button variant="outlined" component={Link} to="/contact" size="large">Contact</Button>
