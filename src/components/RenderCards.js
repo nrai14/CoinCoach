d89538db16d047a4b2372bef1d27af08aa1fc17c
SLM <sabrina.martorelli@gmail.com>
2023-02-22 18:38:08 +0000

    Adds comments to Render Cards component and amends reponsive version

--- src/components/RenderCards.js
+++ src/components/RenderCards.js
@@ -10,6 +10,7 @@ import Grid from '@mui/material/Grid';
 
 function RenderCards() {
 
+
   const [Pot1, setPot1] = useState(0);
   const [Pot2, setPot2] = useState(0);
   const [Pot3, setPot3] = useState(0);
@@ -17,7 +18,7 @@ function RenderCards() {
   useEffect(() => {
     const Pots = JSON.parse(localStorage.getItem('pots'));
 
-
+    // Gets information of Balance, Income and Savings for the cards with videos
     if (Pots) {
       setPot1(Pots[11].value);
       setPot2(Pots[7].value);
@@ -38,9 +39,7 @@ function RenderCards() {
                 <video
                   autoPlay
                   loop
-                  muted
-
-                >
+                  muted>
                   <source
                     src="https://static.vecteezy.com/system/resources/previews/002/039/650/mp4/woman-holding-smartphone-and-credit-ard-free-video.mp4"
                     type="video/mp4"
@@ -49,7 +48,7 @@ function RenderCards() {
               </CardCover>
               <CardContent>
                 <Typography
-                  fontSize={30}
+                  fontSize={{xs:20, sm:30}}
                   fontWeight="lg"
                   textColor="#001C55"
                   mt={{ xs: 12, sm: 18 }}
@@ -58,14 +57,13 @@ function RenderCards() {
               </CardContent>
             </Card>
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={6}>
             <Card sx={{ maxWidth: 250, flexGrow: 1 }}>
               <CardCover>
                 <video
                   autoPlay
                   loop
-                  muted
-                >
+                  muted>
                   <source
                     src="https://static.vecteezy.com/system/resources/previews/001/803/901/mp4/euro-coins-falling-into-a-glass-jar-free-video.mp4"
                     type="video/mp4"
@@ -74,29 +72,23 @@ function RenderCards() {
               </CardCover>
               <CardContent>
                 <Typography
-
-                  fontSize={30}
+                   fontSize={{xs:20, sm:30}}
                   fontWeight="lg"
                   textColor="#fff"
-
                   mt={{ xs: 12, sm: 18 }}
-
                 >Income : £{Pot2}
                 </Typography>
               </CardContent>
             </Card>
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={6}>
             <Card sx={{ maxWidth: 250, flexGrow: 1 }}>
               <CardCover >
                 <video
                   autoPlay
                   loop
-                  muted
-
-                >
+                  muted>
                   <source
-
                     src="https://static.vecteezy.com/system/resources/previews/002/015/167/mp4/hand-puts-coins-in-a-piggy-bank-free-video.mp4"
                     type="video/mp4"
                   />
@@ -104,11 +96,9 @@ function RenderCards() {
               </CardCover>
               <CardContent>
                 <Typography
-
-                  fontSize={30}
+                   fontSize={{xs:20, sm:30}}
                   fontWeight="lg"
                   textColor="#fff"
-
                   mt={{ xs: 12, sm: 18 }}
                 >Savings : £{Pot3}
                 </Typography>
