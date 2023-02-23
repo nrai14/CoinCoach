67afe3b9319520c48383ac4455c506de7f449d02
SLM <sabrina.martorelli@gmail.com>
2023-02-23 13:15:25 +0000

    Adds error boundary for cryptoInfo component and goes back to old code

--- src/components/CryptoInfo.js
+++ src/components/CryptoInfo.js
@@ -1,68 +1,25 @@
+import React from "react";
 import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import { CardActionArea } from '@mui/material';
-
-import moment from 'moment';
-
-
 
 function CryptoInfo({ cryptoData }) {
-
   return (
-    <>
-   
+ 
+    <div>
       <Typography variant='h4' align='center' pt="50px">Short Crypto News </Typography>
-      <Grid
-        container
-        spacing={1}
-        direction="column"
-        alignItems="center"
-        justify="center"
-      >
-        {cryptoData.slice(0, 5).map((article, index) => (
-
-          <Grid item xs={6}>
-            <Card sx={{maxWidth: 445, mb: 3, mt: 3 }}
-            
-            >
-              <CardActionArea
-                target="_blank"
-                href={article.url}
-                component="a"
-              >
-                <CardMedia
-                  component="img"
-                  height="140"
-                 //image={article.urlToImage}
-                  image="https://static.vecteezy.com/system/resources/previews/007/459/324/non_2x/bitcoin-btc-group-included-cryptocurrency-and-stock-chart-candlestick-down-trend-lose-stock-on-stack-of-coins-use-technology-cryptocurrency-blockchain-close-up-coin-free-photo.jpg"
-                  alt="News image"
-                />
-    
-                <CardContent>
-                  <Typography variant="body2" >
-                    {moment(article.publishedAt).format('DD/MM/YYYY')}
-                  </Typography>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {article.title}
-                  </Typography>
-                  <Typography variant="body1" >
-                    {article.author}
-                  </Typography>
-                  <Typography variant="body2" >
-                    {article.description}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-     
-    </>
+      {cryptoData.slice(0, 5).map((article, index) => (
+        <div key={index}>
+         
+          <h4>{article.title}</h4>
+          <h5>{article.author}</h5>  
+          {/* <img src={article.urlToImage} alt='news image'/> */}
+          <p>{article.description}</p>     
+          <h5>{new Date(article.publishedAt).toLocaleDateString("en-GB")}</h5>
+          <a href={article.url}>Select here to read news</a>
+          <hr></hr>
+        </div>
+      ))}
+    </div>  
   );
 }
 
-export default CryptoInfo; 
+export default CryptoInfo;
--- src/pages/CryptoTracker.js
+++ src/pages/CryptoTracker.js
@@ -1,9 +1,34 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import OpenAi from '../components/OpenAi';
 import CryptoInfo from '../components/CryptoInfo';
 
 import Box from "@mui/material/Box";
+import Typography from '@mui/material/Typography';
+
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { hasError: false };
+    }
+  
+    static getDerivedStateFromError(error) {
+      // Update state so the next render will show the fallback UI.
+      return { hasError: true };
+    }
+  
+   
+  
+    render() {
+      if (this.state.hasError) {
+        // You can render any custom fallback UI
+        return <Typography variant='h5' align='center'>The API is not working at the moment. Please try again later.</Typography>;
+      }
+  
+      return this.props.children; 
+    }
+  }
+
 
 function CryptoTracker() {
 
@@ -35,9 +60,9 @@ function CryptoTracker() {
         margin: "0 auto" }}>
 
         <OpenAi cryptoData={cryptoData.slice(0, 5)} />
-
+        <ErrorBoundary>
         <CryptoInfo cryptoData={cryptoData} />
-        
+        </ErrorBoundary>
         </Box>
             
         </>
